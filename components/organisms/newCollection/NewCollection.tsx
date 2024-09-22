7d63487a65468c5e45b30903a10bd69d078f7baf
nadimpallibhargav <bhargavraju98@gmail.com>
2024-09-22 16:59:12 +0530

    💄line height

--- components/organisms/newCollection/NewCollection.tsx
+++ components/organisms/newCollection/NewCollection.tsx
@@ -10,7 +10,7 @@ const NewCollection = () => {
       >
         <div className='container translate-y-[-50%]'>
           <div className='flex justify-end items-center'>
-            <div className='overflow-hidden w-[66px] whitespace-nowrap flex marquee bg-white-1 text-black-1 text-[1rem] font-bold rounded-[4px] py-[6px]'>
+            <div className='overflow-hidden w-[66px] whitespace-nowrap flex items-center leading-[12px] marquee bg-white-1 text-black-1 text-[1rem] font-bold rounded-[4px] py-[6px]'>
               <span>NEW - NEW-</span>
               <span aria-hidden='true'>NEW - NEW-</span>
             </div>
