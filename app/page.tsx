911238957045a3150c21c1f4c0ce118124978945
Rahul <rahulyadav1209ts@gmail.com>
2024-11-14 17:02:22 +0530

    build issue

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import WatchAnimation from "@/components/organisms/watchAnimation/watchAnimation
 import Upgrade from "@/components/organisms/upgrade/Upgrade";
 import QaulityMarque from "@/components/organisms/qualityMarque/QualityMarque";
 import Gallery from "@/components/organisms/gallery/Gallery";
-import Articles from "@/components/organisms/articles/Articles";
+// import Articles from "@/components/organisms/articles/Articles";
 import GetInTouch from "@/components/organisms/getInTouch/GetInTouch";
 
 export default function Home() {
